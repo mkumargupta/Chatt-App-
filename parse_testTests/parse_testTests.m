0749fde4ee879b6b21c4cc5ab78d04cb37ebf332
Saurabh Mishra <srbmishra91@gmail.com>
2014-06-19 16:04:25 +0530

    Initial Commit

---
+++ parse_testTests/parse_testTests.m
@@ -0,0 +1,34 @@
+//
+//  parse_testTests.m
+//  parse_testTests
+//
+//  Created by Saurabh Mishra on 6/19/14.
+//  Copyright (c) 2014 iquince. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface parse_testTests : XCTestCase
+
+@end
+
+@implementation parse_testTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
